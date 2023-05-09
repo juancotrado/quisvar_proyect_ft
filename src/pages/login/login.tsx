504cc75bce1630d94f8ba8138c9ba237dc1924c4
Jhon Carlos <gpro1pro@gmail.com>
2023-05-09 11:43:57 -0500

    config husky and debounce input

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,34 +1,38 @@
-import { ChangeEvent, FormEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 
-type Props = {}
+type Props = {};
 
 const login = (props: Props) => {
-  // const [username, setUsername] = useState<string>('gaaa')
-  // const [password, setPassword] = useState<string>('gaa')
-  const [data, setData] = useState({})
-
-
-  const sendForm = (e:FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const [data, setData] = useState({});
+  const debounceRef = useRef<NodeJS.Timeout>();
+  const sendForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     console.log('Formulario enviado');
-    
-  }
-  const handleLogin = (e:ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target
-    setData({
-      ...data, [name]:value
-    })
-    console.log(data)
-  }
+  };
+  const handleLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    console.log(debounceRef.current);
+    if (debounceRef.current) clearTimeout(debounceRef.current);
+    debounceRef.current = setTimeout(() => {
+      console.log({
+        ...data,
+        [name]: value,
+      });
+      setData({
+        ...data,
+        [name]: value,
+      });
+    }, 1000);
+  };
   return (
     <div>
       <form onSubmit={sendForm}>
-        <input type="text"  onChange={handleLogin} name="username"/>
-        <input type="password" onChange={handleLogin} name="password"/>
+        <input type="text" onChange={handleLogin} name="username" />
+        <input type="password" onChange={handleLogin} name="password" />
         <button type="submit">Ingresar</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default login;

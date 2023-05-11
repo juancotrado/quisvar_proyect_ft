8d4ef01aa1c75795bae158945bf98e981bab4a9e
Jim <j1mer0528@gmail.com>
2023-05-11 14:47:35 -0500

    Added navbar and home screen

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './header.css';
-
 import { useNavigate } from 'react-router-dom';
+
+const items = [
+  { title: 'Inicio', link: '/home' },
+  { title: 'Tareas', link: '/dashboard' },
+  { title: 'Areas', link: '/dashboard' },
+];
+const icons = [
+  { name: '/svg/bell.svg', link: '/dashboard' },
+  { name: '/svg/question-circle.svg', link: '/dashboard' },
+  { name: '/svg/icon.svg', link: '/dashboard' },
+  { name: '/svg/Profile Avatar.svg', link: '/dashboard' },
+];
 
 const Header = () => {
   const navigate = useNavigate();
@@ -9,18 +21,73 @@ const Header = () => {
     localStorage.removeItem('token');
     navigate('login');
   };
+  useEffect(() => {
+    const links = document.querySelectorAll(
+      '.item-text'
+    ) as NodeListOf<HTMLAnchorElement>;
+
+    links.forEach(link => {
+      if (link.href === window.location.href) {
+        link.classList.add('active');
+      }
+
+      const handleClick = (event: MouseEvent) => {
+        event.preventDefault();
+        link.classList.add('clicked');
+        window.location.href = link.href;
+      };
+
+      const handleMouseOver = () => {
+        if (!link.classList.contains('clicked')) {
+          link.classList.add('active');
+        }
+      };
+
+      const handleMouseOut = () => {
+        if (!link.classList.contains('clicked')) {
+          link.classList.remove('active');
+        }
+      };
+
+      link.addEventListener('click', handleClick);
+      link.addEventListener('mouseover', handleMouseOver);
+      link.addEventListener('mouseout', handleMouseOut);
+
+      return () => {
+        link.removeEventListener('click', handleClick);
+        link.removeEventListener('mouseover', handleMouseOver);
+        link.removeEventListener('mouseout', handleMouseOut);
+      };
+    });
+  }, []);
+
   return (
-    <header className="container">
+    <header className="nav-container">
       <figure className="header__figure">
         <img src="/img/quisvar_logo.png" alt="" />
       </figure>
+      <ul className="items-list">
+        {items.map((item, id) => (
+          <li className="item" key={id}>
+            <a href={item.link} className="item-text">
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
       <input
         className="input__search"
         type="text"
         placeholder="Search"
         value=""
       ></input>
-      <button onClick={handleLogout}>Logout</button>
+      <ul className="icons-list">
+        {icons.map((icon, id) => (
+          <li key={id} onClick={handleLogout}>
+            <img src={icon.name} alt="" className="icon" />
+          </li>
+        ))}
+      </ul>
     </header>
   );
 };
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import './home.css';
 // interface HomePageProps {};
-
+const name = 'Diego';
 const home = () => {
-  return <div>home</div>;
+  return (
+    <div className="container-home">
+      <div className="title-content">
+        <h1 className="h1-left">Bienvenido</h1>
+        <h1 className="h1-right">{name}</h1>
+      </div>
+      <p className="paragraph">
+        ¡Bienvenido a nuestro sistema de asignación de tareas! Aquí podrás
+        asignar y gestionar tareas de manera eficiente en tan solo unos pocos
+        clics. Simplifica tu vida y aumenta la productividad de tu equipo con
+        nuestra plataforma.
+      </p>
+      <div className="btn-section">
+        <button className="login-btn">Tus tareas</button>
+        <button className="login-btn">Lista de Tareas</button>
+      </div>
+    </div>
+  );
 };
 
 export default home;

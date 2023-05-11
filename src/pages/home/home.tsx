109dcffc8c168c4e8822d7fdf6b1e1355c68dbfd
jeanticona <jecar324@gmail.com>
2023-05-10 23:35:05 -0500

    added task card component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import TaskCard from '../../components/shared/card/TaskCard';
 
 // interface HomePageProps {};
 
 const home = () => {
-  return (
-    <div>
-      home
-      <div
-        style={{
-          padding: 5,
-          width: '300px',
-          height: '300px',
-          backgroundColor: '#e3e3e3',
-        }}
-      >
-        <TaskCard />
-      </div>
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default home;

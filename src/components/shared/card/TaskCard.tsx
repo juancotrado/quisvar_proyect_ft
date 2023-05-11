c4015a3135232f2deab930702817272c63abfe78
jeanticona <jecar324@gmail.com>
2023-05-10 22:44:46 -0500

    configured global vars css

---
+++ src/components/shared/card/TaskCard.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import './taskcard.css';
+const TaskCard = () => {
+  return (
+    <div className="task-class">
+      <div className="task-header-card">Recursos Humanos</div>
+      <span className="icon-card task-done">
+        <div>icon</div>
+      </span>
+      <p>TaskCards</p>
+    </div>
+  );
+};
+
+export default TaskCard;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import TaskCard from '../../components/shared/card/TaskCard';
 
-type Props = {}
+// interface HomePageProps {};
 
-const home = (props: Props) => {
+const home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div>
+      home
+      <div
+        style={{
+          padding: 5,
+          width: '300px',
+          height: '300px',
+          backgroundColor: '#e3e3e3',
+        }}
+      >
+        <TaskCard />
+      </div>
+    </div>
+  );
+};
 
-export default home
+export default home;

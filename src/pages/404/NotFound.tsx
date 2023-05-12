535646ac3f31d85d97c11d525d2696e9f3ba1c45
unknown <70176266juan@gmail.com>
2023-05-12 17:35:49 -0500

    notFound component finish

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,5 +1,33 @@
+import './notFound.css';
+import { NavLink } from 'react-router-dom';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="notFound">
+      <figure className="notFound-figure">
+        <img alt="" src="/img/rocket.png" className="notFound-figure-img" />
+        <div className="notFound-contain-logo">
+          <img src="/img/quisvar_logo2.png" alt="" />
+        </div>
+      </figure>
+      <div className="label-group">
+        <label className="notFound-label">Ooops! - 404</label>
+        <p className="notFound-paragraph">
+          Lo sentimos, parece que no podemos encontrar lo
+        </p>
+        <p className="notFound-paragraph">que estás buscando.</p>
+        <p className="notFound-paragraph">
+          Ha aterrizado en una URL que no parece existir.
+        </p>
+        {/* <button onClick={handleReturn} type="submit" className="notFound-btn">
+          
+        </button> */}
+        <NavLink to={'login'} className={'notFound-btn'}>
+          VOLVER
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;

6679b80380852eb03f05f44419356f279a45f8c5
Jhon Carlos <gpro1pro@gmail.com>
2023-05-13 02:18:06 -0500

    config home style

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from 'react';
 import './home.css';
+import { useNavigate } from 'react-router-dom';
 // interface HomePageProps {};
 
 const Home = () => {
   const [firstName, setFirstName] = useState('');
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     const name = localStorage.getItem('name') || 'unknow';
     setFirstName(name);
   }, []);
 
+  const handleNavigate = () => navigate('/areas');
   return (
-    <div className="container-home">
-      <div className="title-content">
-        <h1>
-          BIENVENIDO <span>{firstName}</span>
-        </h1>
-      </div>
+    <div className="home">
+      <h1 className="home-title">
+        BIENVENIDO <span className="title-content-span">{firstName}</span>
+      </h1>
       <p className="paragraph">
         ¡Bienvenido a nuestro sistema de asignación de tareas! Aquí podrás
         asignar y gestionar tareas de manera eficiente en tan solo unos pocos
@@ -24,7 +27,9 @@ const Home = () => {
       </p>
       <div className="btn-section">
         <button className="home-btn btn-color-1">Tus tareas</button>
-        <button className="home-btn btn-color-2">Lista de Tareas</button>
+        <button className="home-btn btn-color-2" onClick={handleNavigate}>
+          Áreas
+        </button>
       </div>
     </div>
   );

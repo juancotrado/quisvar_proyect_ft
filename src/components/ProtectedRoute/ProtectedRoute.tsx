a8df8193aa12d15ffe07dbf92ca2716315471e8b
Jhon Carlos <gpro1pro@gmail.com>
2023-05-13 02:18:43 -0500

    config variable

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,10 +2,11 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { Header } from '..';
 
 export const ProtectedRoute = () => {
-  const isLogged = localStorage.getItem('token');
+  // const isLogged = localStorage.getItem('token');
+  const isLogged = true;
   if (isLogged) {
     return (
-      <div style={{ height: '100vh', backgroundColor: '#f7f7f7' }}>
+      <div style={{ height: '100vh', backgroundColor: '#f5f5f5' }}>
         <Header />
         <Outlet />
       </div>

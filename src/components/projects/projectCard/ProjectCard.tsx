d11c3502eaca54ff7d943ccfdc59ee6742287432
Jhon Carlos <gpro1pro@gmail.com>
2023-05-13 02:14:03 -0500

    create project card component

---
+++ src/components/projects/projectCard/ProjectCard.tsx
@@ -0,0 +1,32 @@
+import './projectCard.css';
+
+const ProjectCard = ({ project }: any) => {
+  return (
+    <div className="project-card">
+      <figure className="project-card-figure">
+        <img src="/svg/project_icon.svg" alt="" />
+      </figure>
+      <div className="project-card-main">
+        <div className="projec-card-header">
+          <h3 className="project-card-subtitle">{project.name}</h3>
+          <p className="project-card-date">Fecha Limite: {project.untilDate}</p>
+        </div>
+        <h4 className="project-card-cordinator">
+          COORDINADOR: {project.Users.profile.firstName}{' '}
+          {project.Users.profile.lastName}
+        </h4>
+        <p className="project-card-description">
+          {project.description
+            ? project.description
+            : 'CREACION DEL SERVICIO DE PRÁCTICA DEPORTIVA Y/O RECREATIVA EN LA COMUNIDAD CAMPESINA DE KALAHUALA DISTRITO DE ASILLO DE LA PROVINCIA DE AZANGARO DEL DEPARTAMENTO DE PUNO.'}
+        </p>
+        <div className="project-card-footer">
+          <p className="project-card-task">TOTAL DE TAREAS: 1</p>
+          <p className="project-card-show">VER MAS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;

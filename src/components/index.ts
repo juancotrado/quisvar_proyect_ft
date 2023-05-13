64de860e7398573958d2fbfbd46909596c2ae9a3
castillo <gpro1pro@gmail.com>
2023-05-13 17:16:33 -0500

    change route

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
+import { ProtectedRoute } from './protectedRoute/ProtectedRoute';
 import Alert from './shared/alert/Alert';
 import Header from './shared/header/Header';
 
-export { Alert, Header };
+export { Alert, Header, ProtectedRoute };

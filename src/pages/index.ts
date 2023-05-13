1603e0609e76a7c0d64bccc3c24548bdec2fc037
Jhon Carlos <gpro1pro@gmail.com>
2023-05-13 02:19:52 -0500

    change component name

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,5 @@ import Login from './login/index';
 import Home from './home/index';
 import Dashboard from './dashboard/index';
 import NotFound from './404/NotFound';
-import Task from './task/task';
+import Task from './task/Task';
 export { Login, Home, Dashboard, NotFound, Task };

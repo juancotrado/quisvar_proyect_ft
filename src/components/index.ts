a51d4be4f2323d80d440d79adea36dd5b8dab0ae
Jim <j1mer0528@gmail.comholagit config --global user.email j1mer0528@gmail.com>
2023-05-15 12:27:40 -0500

    fix: gap style

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,13 @@ import Header from './shared/header/Header';
 import CardRegisterUser from './cardRegister/CardRegisterUser';
 import Input from './shared/Input/Input';
 import { ProtectedRoute } from './ProtectedRoute/ProtectedRoute';
+import CardRegisterTask from './cardTaskRegister/CardRegisterTask';
 
-export { Alert, Header, CardRegisterUser, Input, ProtectedRoute };
+export {
+  Alert,
+  Header,
+  CardRegisterUser,
+  Input,
+  ProtectedRoute,
+  CardRegisterTask,
+};

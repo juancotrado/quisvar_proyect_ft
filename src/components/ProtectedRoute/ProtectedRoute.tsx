14c1c4e17aa3ed0b776409ce15bc11b777601a63
Jhon Carlos <gpro1pro@gmail.com>
2023-05-15 21:55:49 -0500

    active bearer token auth

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,8 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { Header } from '..';
 
 export const ProtectedRoute = () => {
-  // const isLogged = localStorage.getItem('token');
-  const isLogged = true;
+  const isLogged = localStorage.getItem('token');
   if (isLogged) {
     return (
       <div style={{ height: '100vh', backgroundColor: '#f5f5f5' }}>

e1aea013bc7af6ac13f6d3f14fd405afdfcc1613
Jhon Carlos <gpro1pro@gmail.com>
2023-05-16 18:14:17 -0500

    delete loader from this loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { useEffect, useState } from 'react';
 import Navigation from './routes/Navigation';
 import Loader from './components/shared/loader/Loader';
 import { SnackbarUtilitiesConfigurator } from './utils/SnackbarManager';
-import { isLoader$ } from './services/axiosInstance';
 
 function App() {
-  const [isLoader, setisLoader] = useState(false);
-
-  isLoader$.subscribe({
-    next: value => setisLoader(value),
-  });
-
   return (
     <>
       <SnackbarUtilitiesConfigurator />
       <Navigation />
-      <Loader isLoading={isLoader} />
+      <Loader />
     </>
   );
 }

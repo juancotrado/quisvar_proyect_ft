8e81c3b5c61ba5417d6a7090ae4935bb2615b263
Jhon Carlos <gpro1pro@gmail.com>
2023-05-17 13:25:40 -0500

    fixed route name

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
-import Login from './login/index';
-import Home from './home/index';
 import Dashboard from './dashboard/index';
 import NotFound from './404/NotFound';
 import Task from './task/Task';
+import Login from './login/Login';
+import Home from './home/Home';
+
 export { Login, Home, Dashboard, NotFound, Task };

84f78d5067a02338485a698d9ceaefea050ed1c9
Jhon Carlos <gpro1pro@gmail.com>
2023-05-19 01:08:40 -0500

    get user session id and session name

--- src/hooks/useRole.tsx
+++ src/hooks/useRole.tsx
@@ -2,14 +2,18 @@ import { useEffect, useState } from 'react';
 
 const useRole = () => {
   const [role, setRole] = useState<string | null>(null);
+  const [id, setId] = useState<number>(0);
+  const [name, setName] = useState<string>('');
 
   useEffect(() => {
     const userInfo = localStorage.getItem('personalData');
     if (typeof userInfo !== 'string') return;
     const _user = JSON.parse(userInfo);
     setRole(_user.role);
+    setId(_user.id);
+    setName(_user.name);
   }, []);
-  return { role };
+  return { role, id, name };
 };
 
 export default useRole;

a9655eaff29d79e1409e105f38584d1aa7e250c8
Jhon Castillo <jcastillo@dlya.com.uy>
2023-05-23 21:50:37 -0500

    coment code

--- src/pages/task/task.tsx
+++ src/pages/task/task.tsx
@@ -12,101 +12,101 @@ import Button from '../../components/shared/button/Button';
 import { isOpenModal$ } from '../../services/sharingSubject';
 
 const Task = () => {
-  const [tasks, setTasks] = useState<TaskType[] | null>(null);
-  const [getTaskData, setGetTaskData] = useState<TaskType | null>(null);
-  const socket = useSocket();
-  const { id } = useParams();
-  const { role } = useRole();
+  //   const [tasks, setTasks] = useState<TaskType[] | null>(null);
+  //   const [getTaskData, setGetTaskData] = useState<TaskType | null>(null);
+  //   const socket = useSocket();
+  //   const { id } = useParams();
+  //   const { role } = useRole();
 
-  useEffect(() => {
-    axiosInstance
-      .get(`/projects/${id}`)
-      .then(res => {
-        setTasks(res.data.tasks);
-        socket.emit('data', res.data.tasks);
-      })
-      .catch(err => console.log(err));
+  //   useEffect(() => {
+  //     axiosInstance
+  //       .get(`/projects/${id}`)
+  //       .then(res => {
+  //         setTasks(res.data.tasks);
+  //         socket.emit('data', res.data.tasks);
+  //       })
+  //       .catch(err => console.log(err));
 
-    return () => {
-      socket.disconnect();
-    };
-  }, [id, socket]);
+  //     return () => {
+  //       socket.disconnect();
+  //     };
+  //   }, [id, socket]);
 
-  useEffect(() => {
-    socket.on('data', (tasksSocket: TaskType[]) => {
-      if (tasks && tasksSocket[0].projectId === tasks[0].projectId) {
-        setTasks(tasksSocket);
-      }
-    });
-  }, [socket, tasks]);
+  //   useEffect(() => {
+  //     socket.on('data', (tasksSocket: TaskType[]) => {
+  //       if (tasks && tasksSocket[0].projectId === tasks[0].projectId) {
+  //         setTasks(tasksSocket);
+  //       }
+  //     });
+  //   }, [socket, tasks]);
 
-  useEffect(() => {
-    socket.on('add-task', newTask => {
-      if (!tasks) return;
-      setTasks([...tasks, newTask]);
-    });
-  }, [socket, tasks]);
+  //   useEffect(() => {
+  //     socket.on('add-task', newTask => {
+  //       if (!tasks) return;
+  //       setTasks([...tasks, newTask]);
+  //     });
+  //   }, [socket, tasks]);
 
-  useEffect(() => {
-    socket.on('update-task', updateTask => {
-      if (!tasks) return;
-      const newTasks = tasks?.map(task =>
-        task.id == updateTask.id ? updateTask : task
-      );
-      setTasks(newTasks);
-    });
-  }, [socket, tasks]);
+  //   useEffect(() => {
+  //     socket.on('update-task', updateTask => {
+  //       if (!tasks) return;
+  //       const newTasks = tasks?.map(task =>
+  //         task.id == updateTask.id ? updateTask : task
+  //       );
+  //       setTasks(newTasks);
+  //     });
+  //   }, [socket, tasks]);
 
-  useEffect(() => {
-    socket.on('delete-task-success', id => {
-      if (!tasks) return;
-      const newTasks = tasks.filter(task => task.id !== id);
-      setTasks(newTasks);
-    });
-  }, [socket, tasks]);
+  //   useEffect(() => {
+  //     socket.on('delete-task-success', id => {
+  //       if (!tasks) return;
+  //       const newTasks = tasks.filter(task => task.id !== id);
+  //       setTasks(newTasks);
+  //     });
+  //   }, [socket, tasks]);
 
-  const editTaskStatus = (
-    id: number,
-    status: string,
-    newEmployees: Employees[] = []
-  ) => {
-    if (!tasks) return;
-    const personalData = localStorage.getItem('personalData');
-    if (!personalData) return;
-    const personalDataParse = JSON.parse(personalData);
+  //   const editTaskStatus = (
+  //     id: number,
+  //     status: string,
+  //     newEmployees: Employees[] = []
+  //   ) => {
+  //     if (!tasks) return;
+  //     const personalData = localStorage.getItem('personalData');
+  //     if (!personalData) return;
+  //     const personalDataParse = JSON.parse(personalData);
 
-    const newTasks = tasks?.map(task =>
-      task.id == id ? { ...task, status, employees: newEmployees } : task
-    );
-    socket.emit('update-status', {
-      id,
-      body: { status },
-      userId: personalDataParse.id,
-    });
-    socket.emit('data', newTasks);
-    setTasks(newTasks);
-  };
+  //     const newTasks = tasks?.map(task =>
+  //       task.id == id ? { ...task, status, employees: newEmployees } : task
+  //     );
+  //     socket.emit('update-status', {
+  //       id,
+  //       body: { status },
+  //       userId: personalDataParse.id,
+  //     });
+  //     socket.emit('data', newTasks);
+  //     setTasks(newTasks);
+  //   };
 
-  const addNewTask = () => {
-    clearDataInModal();
-    isOpenModal$.setSubject = true;
-  };
+  //   const addNewTask = () => {
+  //     clearDataInModal();
+  //     isOpenModal$.setSubject = true;
+  //   };
 
-  const createTask = (task: TaskType) => {
-    socket.emit('create-task', task);
-    clearDataInModal();
-  };
-  const editTask = (task: TaskType) => {
-    socket.emit('edit-task', task);
-    clearDataInModal();
-  };
+  //   const createTask = (task: TaskType) => {
+  //     socket.emit('create-task', task);
+  //     clearDataInModal();
+  //   };
+  //   const editTask = (task: TaskType) => {
+  //     socket.emit('edit-task', task);
+  //     clearDataInModal();
+  //   };
 
-  const deleteTask = (id: number) => {
-    socket.emit('delete-task', id);
-  };
-  const clearDataInModal = () => setGetTaskData(null);
+  //   const deleteTask = (id: number) => {
+  //     socket.emit('delete-task', id);
+  //   };
+  //   const clearDataInModal = () => setGetTaskData(null);
 
-  const handleGetTaskData = (getTask: TaskType) => setGetTaskData(getTask);
+  //   const handleGetTaskData = (getTask: TaskType) => setGetTaskData(getTask);
 
   return (
     <div className="tasks container">
@@ -114,19 +114,19 @@ const Task = () => {
         <h1 className="main-title">
           LISTA DE <span className="main-title-span">TAREAS</span>
         </h1>
-        {role !== 'EMPLOYEE' && (
+        {/* {role !== 'EMPLOYEE' && (
           <Button
             text="Agregar"
             icon="plus"
             className="btn-add"
             onClick={addNewTask}
           />
-        )}
+        )} */}
       </div>
 
       <section className="tasks-section-container">
         <div className="container-task container-unresolved">
-          <h3>Por hacer</h3>
+          {/* <h3>Por hacer</h3>
           {tasks &&
             tasks
               .filter(({ status }) => status === 'UNRESOLVED')
@@ -138,11 +138,11 @@ const Task = () => {
                   handleGetTaskData={handleGetTaskData}
                   deleteTask={deleteTask}
                 />
-              ))}
+              ))} */}
         </div>
         <div className="container-task container-process">
           <h3>Haciendo</h3>
-          {tasks &&
+          {/* {tasks &&
             tasks
               .filter(({ status }) => status === 'PROCESS')
               .map(task => (
@@ -153,11 +153,11 @@ const Task = () => {
                   handleGetTaskData={handleGetTaskData}
                   deleteTask={deleteTask}
                 />
-              ))}
+              ))} */}
         </div>
         <div className="container-task container-done">
           <h3>Hecho</h3>
-          {tasks &&
+          {/* {tasks &&
             tasks
               .filter(({ status }) => status === 'DONE')
               .map(task => (
@@ -168,7 +168,7 @@ const Task = () => {
                   handleGetTaskData={handleGetTaskData}
                   deleteTask={deleteTask}
                 />
-              ))}
+              ))} */}
 
           {/* {tasks && <TaskCard task={tasks} />} */}
         </div>
@@ -178,11 +178,11 @@ const Task = () => {
           Usuarios Conectados: {userOnline}
         </h4>
       </div> */}
-      <ModalFormTask
+      {/* <ModalFormTask
         createTask={createTask}
         getTaskData={getTaskData}
         editTask={editTask}
-      />
+      /> */}
     </div>
   );
 };

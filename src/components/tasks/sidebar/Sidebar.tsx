38eb2ebded05cecebf40d4bdebf9fb8be75703cd
Jhon Castillo <gpro1pro@gmail.com>
2023-05-26 01:19:59 -0500

    config sidebar

---
+++ src/components/tasks/sidebar/Sidebar.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { WorkArea } from '../../../types/types';
+import './sidebar.css';
+
+interface SidebarProps {
+  workArea: WorkArea;
+  settingSubTasks: (id: number) => void;
+}
+
+const Sidebar = ({ workArea, settingSubTasks }: SidebarProps) => {
+  const { name, indexTasks } = workArea;
+
+  const [isShow, setIsShow] = useState<boolean>(true);
+
+  const handleShow = () => setIsShow(!isShow);
+  return (
+    <aside className={`aside ${isShow && 'aside-show'}`}>
+      <h2 className="aside-title">{name}</h2>
+      <div className="asilde-slice">
+        <ul className="aside-dropdown">
+          {indexTasks.map(indexTask => (
+            <li key={indexTask.id} className="aside-dropdown-list">
+              <div className="aside-dropdown-section">
+                <img
+                  src="/svg/reports.svg"
+                  alt="reportes"
+                  className="aside-dropdown-icon"
+                />
+                <span>{indexTask.name}</span>
+                <img src="/svg/down.svg" className="aside-dropdown-arrow" />
+                <input type="checkbox" className="aside-dropdown-check" />
+              </div>
+              <div className="aside-dropdown-content">
+                <ul className="aside-dropdown-sub">
+                  {indexTask.tasks.map(task => (
+                    <li
+                      key={task.id}
+                      className="aside-dropdown-sub-list"
+                      onClick={() => settingSubTasks(task.id)}
+                    >
+                      {task.name.toLowerCase()}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="aside-slide" onClick={handleShow}>
+        <img
+          src="/svg/down-white.svg"
+          alt="reportes"
+          className={`aside-slide-icon ${isShow && 'aside-slide-icon-rotate'}`}
+        />
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;

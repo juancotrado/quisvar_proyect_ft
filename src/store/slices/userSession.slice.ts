89df82a9841b9680905f9636c6be56877aca28d4
Jhon Castillo <gpro1pro@gmail.com>
2023-05-27 12:21:59 -0500

    config userSession slice

--- src/store/slices/userSession.slice.ts
+++ src/store/slices/userSession.slice.ts
@@ -3,15 +3,26 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { Users } from '../../types/types';
 import { AppDispatch } from '..';
 import { axiosInstance } from '../../services/axiosInstance';
-type State = Users | null;
+// type State = Users | null;
 
-const INITIAL_STATE = null as State;
+const INITIAL_STATE: Users = {
+  id: 0,
+  email: '',
+  password: '',
+  profile: {
+    firstName: '',
+    lastName: '',
+    dni: '',
+    phone: '',
+    userId: 0,
+  },
+};
 
 const userSessionSlice = createSlice({
   name: 'userSession',
   initialState: INITIAL_STATE,
   reducers: {
-    setUserSessionGlobal: (state, action: PayloadAction<State>) =>
+    setUserSessionGlobal: (state, action: PayloadAction<Users>) =>
       action.payload,
   },
 });

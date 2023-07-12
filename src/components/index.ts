ab2ad351d534c6a5e1c3957ef99465259389f887
Jhon Castillo <gpro1pro@gmail.com>
2023-07-12 00:47:02 -0500

    create CardSubtaskDone css for this component

--- src/components/index.ts
+++ src/components/index.ts
@@ -16,7 +16,7 @@ import Sidebar from './tasks/sidebar/Sidebar';
 import SubTaskCard from './shared/card/subTaskCard/SubTaskCard';
 import CardTaskInformation from './shared/card/cardTaskInformation/CardTaskInformation';
 import InputRange from './shared/inputRange/InputRange';
-import CardSubtaskDone from './shared/card/cardSubtaskStatus/CardSubtaskDone';
+import CardSubtaskDone from './shared/card/cardSubtaskStatus/cardSubtaskDone/CardSubtaskDone';
 import CardSubtaskHold from './shared/card/cardSubtaskStatus/CardSubtaskHold';
 import CardSubtaskProcess from './shared/card/cardSubtaskStatus/cardSubTaskProcess/CardSubtaskProcess';
 

73d80e4504721583b5d30dafdfdd68d35c9d668f
Jhon Castillo <gpro1pro@gmail.com>
2023-09-06 16:59:19 -0500

    fixed bug in protected route

--- src/components/protected/ProtectedRole/ProtectedRole.tsx
+++ src/components/protected/ProtectedRole/ProtectedRole.tsx
@@ -2,17 +2,22 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 import { RootState } from '../../../store';
 import { UserRoleType } from '../../../types/types';
+import { useEffect, useState } from 'react';
 
 interface ProtectedRoleProps {
   rols: UserRoleType[];
 }
 
 const ProtectedRole = ({ rols }: ProtectedRoleProps) => {
+  const [rol, setRol] = useState<UserRoleType | null>(null);
   const { role } = useSelector((state: RootState) => state.userSession);
+  useEffect(() => {
+    setRol(role);
+  }, [role]);
+
+  if (rol === 'EMPLOYEE' || !rol) return null;
   const isAuthRole = rols.includes(role);
-  if (!isAuthRole) {
-    return <Navigate to="/home" />;
-  }
+  if (!isAuthRole) return <Navigate to="/home" />;
   return <Outlet />;
 };
 

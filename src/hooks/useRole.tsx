0ca7cc89d442c1dffad2c47e71e5aa97fa76a4ff
Jhon Carlos <gpro1pro@gmail.com>
2024-02-05 18:03:33 -0500

    config roles for submenus

--- src/hooks/useRole.tsx
+++ src/hooks/useRole.tsx
@@ -3,14 +3,30 @@ import { RootState } from '../store';
 import { MenuAccess, MenuRole } from '../types';
 import { useLocation } from 'react-router-dom';
 
-const useRole = (typeRol: MenuRole) => {
+const useRole = (
+  typeRol: MenuRole,
+  menu: MenuAccess | null,
+  subMenu?: string
+) => {
   const location = useLocation();
-  const currentUrl = location.pathname.split('/')[1] as MenuAccess;
+  const currentUrl = menu ?? (location.pathname.split('/')[1] as MenuAccess);
 
   const { role } = useSelector((state: RootState) => state.userSession);
-  const hasAccess = role?.menuPoints.some(
-    menuPoint => menuPoint.route === currentUrl && menuPoint.typeRol === typeRol
-  );
+  let hasAccess: boolean | undefined = false;
+  if (subMenu) {
+    const findMenu = role?.menuPoints.find(
+      menuPoint => menuPoint.route === currentUrl
+    );
+    hasAccess = findMenu?.menu?.some(
+      menuPoint => menuPoint.route === subMenu && menuPoint.typeRol === typeRol
+    );
+  } else {
+    hasAccess = role?.menuPoints.some(
+      menuPoint =>
+        menuPoint.route === currentUrl && menuPoint.typeRol === typeRol
+    );
+  }
+
   return { hasAccess };
 };
 

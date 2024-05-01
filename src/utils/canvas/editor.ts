d1e79b4fa9cdf52947fa0d3dd11868f5a2363cae
Diego569 <milord566@gmail.com>
2024-05-01 12:50:07 -0500

    editor default text

--- src/utils/canvas/editor.ts
+++ src/utils/canvas/editor.ts
@@ -1,3 +1,5 @@
-export const INITIAL_VALUE_EDITOR = `<p style="text-align: justify;" >
-Por medio del presente documento me dirijo a usted con la finalidad de hacerle llegar un cordial saludo, y al mismo tiempo comunicarle lo siguiente:
- </p><p>&nbsp;</p>`;
+export const INITIAL_VALUE_EDITOR = `<p>Por medio del presente me dirijo a Ud. Con la finalidad de hacerle llegar un cordial saludo, y al mismo tiempo SOLICITAR EL PAGO DE MI ADELANTO DEL PERIODO 09 DE MARZO AL 05 DE ABRIL DEL 2024 por lo cual:</p>
+<p>ADJUNTO:</p>
+<p><u>\\\\172.16.10.206\\informes\\AREA TECNICA\\PERIODO</u></p>
+<p>Sin otro particular me despido de usted, no sin expresarle las muestras de mi especial consideraci&oacute;n y estima personal.</p>
+<p>&nbsp;</p>`;
